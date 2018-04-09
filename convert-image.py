921648512392089cc361fa49931b3686d22201dd
Jokototo <sebastian-mira@hotmail.com>
2018-04-09 11:21:33 +0200

    Update convert-image.py

--- convert-image.py
+++ convert-image.py
@@ -9,40 +9,18 @@ from io import BytesIO
 # The entry point function can contain up to two input arguments:
 #   Param<dataframe1>: a pandas.DataFrame
 #   Param<dataframe2>: a pandas.DataFrame
-def azureml_main(dataframe1 = None, dataframe2 = None):    
-    response = requests.get("http://johnloeber.com/images/kmeans-mnist/nines/9.png")
+def azureml_main(dataframe1 = None, dataframe2 = None):
+    response = requests.get("https://i.imgur.com/Cx5o0ca.jpg")
     im = Image.open(BytesIO(response.content)).convert('LA')
-    
-    width = float(im.size[0])
-    height = float(im.size[1])
+
     newImage = Image.new('L', (28, 28), (255))  # creates white canvas of 28x28 pixels
-    
-    if width > height:  # check which dimension is bigger
-        # Width is bigger. Width becomes 20 pixels.
-        nheight = int(round((20.0 / width * height), 0))  # resize height according to ratio width
-        if (nheight == 0):  # rare case but minimum is 1 pixel
-            nheight = 1
-            # resize and sharpen
-        img = im.resize((20, nheight), Image.ANTIALIAS).filter(ImageFilter.SHARPEN)
-        wtop = int(round(((28 - nheight) / 2), 0))  # calculate horizontal position
-        newImage.paste(img, (4, wtop))  # paste resized image on white canvas
-    else:
-        # Height is bigger. Heigth becomes 20 pixels.
-        nwidth = int(round((20.0 / height * width), 0))  # resize width according to ratio height
-        if (nwidth == 0):  # rare case but minimum is 1 pixel
-            nwidth = 1
-            # resize and sharpen
-        img = im.resize((nwidth, 20), Image.ANTIALIAS).filter(ImageFilter.SHARPEN)
-        wleft = int(round(((28 - nwidth) / 2), 0))  # caculate vertical pozition
-        newImage.paste(img, (wleft, 4))  # paste resized image on white canvas
-    
-    
+    img = im.resize((28, 28), Image.ANTIALIAS).filter(ImageFilter.SHARPEN) #Reshape image to 28x28
+    newImage.paste(img, (0, 0))  # paste resized image on white canvas
     tv = list(newImage.getdata())  # get pixel values
-    
-    # normalize pixels to 0 and 1. 0 is pure white, 1 is pure black.
+
+    # Inverse pixel values
     tva = [(255 - x) for x in tv]
-    
+
     dataframe1=pd.DataFrame(tva).transpose()
-    colnames=['f'+str(i) for i in range(len(tva))]
-    dataframe1.columns=colnames
+    dataframe1.columns=['f'+ str(i) for i in range(dataframe1.shape[1])]
     return dataframe1
